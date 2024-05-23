8d8996be32a046b619871df81ae77222cc6a360d
1 <diany.kor.2019@mail.ru>
2024-05-24 00:18:10 +0300

    solutions//

--- 1.py
+++ 1.py
@@ -1,14 +1,15 @@
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import messagebox
-from PIL import Image, ImageDraw, ImageGrab
+from PIL import Image
+import os
 
-# cоздаем главное окно класса Tk
 root = tk.Tk()
-root.title("Relax drawing")
+root.title("релакс рисование")
 
 canvas = tk.Canvas(root, width=1000, height=800, bg='white')
 canvas.pack()
+
 current_color = "black"
 
 def change_color(color):
@@ -16,38 +17,38 @@ def change_color(color):
     current_color = color
 
 colors = ["black", "red", "green", "pink", "gray", "blue", "yellow", "violet", "darkgreen", "orange"]
-
 for color in colors:
     color_button = tk.Button(root, bg=color, width=2, command=lambda c=color: change_color(c))
     color_button.pack(side='right')
 
 def draw(event):
     x, y = event.x, event.y
-    canvas.create_oval(x, y, x+4, y+4, fill=current_color, width=0)
+    canvas.create_oval(x, y, x + 4, y + 4, fill=current_color, width=0)
 
 def save_image():
-    x = root.winfo_rootx() + canvas.winfo_x()
-    y = root.winfo_rooty() + canvas.winfo_y()
-    x1 = x + canvas.winfo_width()
-    y1 = y + canvas.winfo_height()
-    # открываем диалоговое окно для сохранения файла и возвращаем путь к сохранённому файлу
     filename = filedialog.asksaveasfilename(defaultextension=".png", filetypes=[("png files", "*.png")])
     if filename:
-        image = ImageGrab.grab(bbox=(x, y, x1, y1))
-        image.save(filename)
-        messagebox.showinfo("сохранение", "Изображение сохранено успешно!")
+        temp_postscript_file = "temp_canvas.eps"
+        canvas.postscript(file=temp_postscript_file, colormode='color')
+
+        with open(temp_postscript_file, 'r') as file:
+            img = Image.open(temp_postscript_file)
+            img.save(filename, "png")
+
+        messagebox.showinfo("сохранение", "изображение успешно сохранено!")
 
 def show_image():
     filename = filedialog.askopenfilename(filetypes=[("png files", "*.png")])
     if filename:
-        image = Image.open(filename)
-        image.show()
+        opened_image = Image.open(filename)
+        opened_image.show()
 
-save_button = tk.Button(root, text="Сохранить", command=save_image)
+save_button = tk.Button(root, text="сохранить", command=save_image)
 save_button.pack(side='left')
 
-show_button = tk.Button(root, text="Показать", command=show_image)
+show_button = tk.Button(root, text="показать", command=show_image)
 show_button.pack(side='left')
 
 canvas.bind("<B1-Motion>", draw)
+
 root.mainloop()
